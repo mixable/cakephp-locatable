d2b9f58ec28ac9d7ef7e5144843b29d85bd4f8ba
Mathias Lipowski <dev@mixable.media>
2021-04-08 14:31:52 +0200

    Fixed behavior

--- src/Model/Behavior/LocatableBehavior.php
+++ src/Model/Behavior/LocatableBehavior.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Locatable\Model\Behavior;
 
+use Cake\Datasource\EntityInterface;
+use Cake\Event\EventInterface;
 use Cake\ORM\Behavior;
 use Cake\ORM\Query;
 
@@ -25,6 +27,11 @@ class LocatableBehavior extends Behavior
         'locatableClass' => 'Coordinates',
         'foreignKey' => 'foreign_key',
         'modelKey' => 'model',
+        'implementedEvents' => [
+            'Model.beforeSave' => 'beforeSave',
+        ],
+        'implementedFinders' => [],
+        'implementedMethods' => [],
     ];
 
     /**
@@ -36,19 +43,37 @@ class LocatableBehavior extends Behavior
     public function initialize(array $config): void
     {
         if (empty($this->getConfig('modelClass'))) {
-            $this->setConfig('modelClass', $this->_table->getAlias());
+            $this->setConfig('modelClass', $this->getTable()->getAlias());
         }
 
         $this->_table->hasOne('Coordinates')
             ->setClassName($this->getConfig('locatableClass'))
             ->setForeignKey($this->getConfig('foreignKey'))
             ->setConditions([
-                $this->getConfig('locatableClass') . '.' . $this->getConfig('modelKey') => $this->_table->getAlias(),
+                $this->getConfig('locatableClass') . '.' . $this->getConfig('modelKey') => $this->getConfig('modelClass'),
             ])
             ->setDependent(true);
 
         $this->_table->Coordinates->belongsTo($this->getConfig('modelClass'))
             ->setClassName($this->getConfig('modelClass'))
             ->setForeignKey($this->getConfig('foreignKey'));
+    }
+
+    /**
+     * beforeSave callback
+     *
+     * @param \Cake\Event\EventInterface $event The beforeSave event that was fired
+     * @param \Cake\Datasource\EntityInterface $entity The entity that is going to be saved
+     * @param \ArrayObject $options The options for the query
+     * @return void
+     */
+    public function beforeSave(EventInterface $event, EntityInterface $entity, \ArrayObject $options)
+    {
+        if ($entity->has('coordinate') && $entity->get('coordinate')->isNew()) {
+            // Automatically set model of association
+            if (!$entity->get('coordinate')->has('model')) {
+                $entity->get('coordinate')->set('model', $this->getTable()->getAlias());
+            }
+        }
     }
 }
